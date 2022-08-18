bb7fbbcd810e42779b99371b9db3534d365d9481
ellawen <iceatdesert@gmail.com>
2022-08-18 22:34:51 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,32 +1,77 @@
 // 載入 express 並建構應用程式伺服器
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose') // 載入 mongoose
+const express = require('express') // (包含取得內建的body-parse)
+const app = express() 
+
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+const Url = require('./models/url')
+
+//把mongoose抽出到config後，在app.js 要引用設定檔
+require('./config/mongoose')
+
+
+// setting body-parser 改寫成 express，也可以直接取得 urlencoded 方法
+app.use(express.urlencoded({ extended: true })) 
+
 
 // setting template engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine('hbs', exphbs({ defaultLayout: 'main' , extname: '.hbs'}))
+//啟用template engine
+app.set('view engine', 'hbs')
 
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+//使用靜態檔案
+app.use(express.static("public"))
+
+
 
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  Url.find() // 取出 Url model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(urls => res.render('index', { urls })) // 將資料傳給 index 樣板 這裡 { urls } 是 { urls: urls } 的縮寫。
+    .catch(error => console.error(error)) // 錯誤處理
+})
+
+app.post('/', (req, res) => {
+  if (!req.body.url) return res.redirect("/") //如果沒有輸入url網址就點擊送出，導回首頁
+  const shortURL = shortenURL(5)
+
+  URL.findOne({ originalURL: req.body.url })
+    .then(data =>
+      data ? data : URL.create({ shortURL, originalURL: req.body.url })
+    )
+    .then(data =>
+      res.render("index", {
+        origin: req.headers.origin,
+        shortURL: data.shortURL,
+      })
+    )
+  //   .catch(error => console.error(error))
+  // const url = req.body.url      // 從 req.body 拿出表單裡的 name 資料
+  // return Url.create({ url })     // 存入資料庫
+  //   .then(() => res.redirect('/')) // 新增完成後導回首頁
+  //   .catch(error => console.log(error))
+})
+
+//
+app.get("/:shortURL", (req, res) => {
+  const { shortURL } = req.params
+
+  URL.findOne({ shortURL })
+    .then(data => {
+      if (!data) {
+        return res.render("error", {
+          errorMsg: "Can't found the URL",
+          errorURL: req.headers.host + "/" + shortURL,
+        })
+      }
+
+      res.redirect(data.originalURL)
+    })
+    .catch(error => console.error(error))
 })
 
 // 設定 port 3000
--- models/url.js
+++ models/url.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const urlSchema = new Schema({
-  name: {
-    type: String, // 資料型別是字串
-    required: true // 這是個必填欄位
+  shortURL: { 
+    type: String, 
+    required: true 
   },
- done: {
-  type: Boolean
-  }
+  originalURL: { 
+    type: String, 
+    required: true 
+  },
 })
 module.exports = mongoose.model('Url', urlSchema)
